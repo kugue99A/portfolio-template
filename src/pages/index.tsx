d7d17d11961437254c602cc8a2f6fa918311a91f
Kugue99A <hisabisa99yohey@gmail.com>
2022-03-06 19:53:48 +0900

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,9 @@
 import type { NextPage } from 'next'
-import { useRouter } from 'next/router';
-import Head from 'next/head'
 
 const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <title>Your Portfolio</title>
-        <meta name="description" content="NUTMEG Member Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;700&display=swap'
-        />
-      </Head>
       <h1>NUTMEG</h1>
     </>
   )
